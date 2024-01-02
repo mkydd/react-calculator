d0442f46d6730c0172cacb483252cf383a68d156
Mike790 <michael.kydd@outlook.com>
2024-01-02 00:33:19 -0500

    add display

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,27 +1,44 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import NumberButton from './NumberButton'
 // import Display from './Display'
 import OperationButton from './OperationButton'
 import EqualButton from './EqualButton'
 import ClearButton from './ClearButton'
+import Display from './Display'
 import '../styles/calculator.css'
 
 function Calculator() {
   const [value1, setValue1] = useState('')
   const [value2, setValue2] = useState('')
   const [operation, setOperation] = useState('')
-  const [output, setOuput] = useState(null)
+  const [output, setOuput] = useState('')
+
+  const hasPageRendered = useRef(false)
+
+  // cant update these values with equalOnClick b/c output is not updated till renrender so value1 will be null
+  useEffect(() => {
+    if (hasPageRendered.current) {
+      if (output) {
+        setValue1(output)
+      }
+    }
+    hasPageRendered.current = true
+  }, [output])
+
+
 
   function numberOnClick(number) {
-    if (operation.length === 0) {
+    if (value1.length === 0) {
+      console.log('value1 + number =', value1 + number)
       setValue1(value1 + number)
     } else {
+      console.log('value2 + number =', value2 + number)
       setValue2(value2 + number)
     }
   }
 
   function operationOnClick(operation) {
-      setOperation(operation)
+      if (value1) {setOperation(operation)}
   }
 
   function equalOnClick(value1, value2, operation) {
@@ -30,34 +47,42 @@ function Calculator() {
 
     switch (operation) {
       case '+':
-        setOuput(num1 + num2)
+        setOuput((num1 + num2).toString())
         break;
       case '-':
-        setOuput(num1 - num2)
+        setOuput((num1 - num2).toString())
         break;
       case '*':
-        setOuput(num1 * num2)
+        setOuput((num1 * num2).toString())
         break;
       case '/':
-        setOuput(num1 / num2)
+        if (num2 === 0){ 
+          clearOnClick()
+        } else {
+          setOuput((num1 / num2).toString())
+        }
         break;
       default:
         console.log(`error: invalid operation (${operation})`)
     }
 
+    setValue2('')
+    setOperation('')
   }
 
   function clearOnClick() {
     setValue1('')
     setValue2('')
     setOperation('')
-    setOuput(null)
+    setOuput('')
   }
-
-
 
   return (
     <div className='calculator'>
+      <div className="row">
+        <Display value1={value1} value2={value2} operation={operation} output={output}/>
+      </div>
+
       <div className="row">
         <ClearButton clearOnClick={clearOnClick}/>
         <OperationButton operation='/' operationOnClick={operationOnClick}/> 
@@ -88,11 +113,6 @@ function Calculator() {
           <NumberButton number='0' numberOnClick={numberOnClick}/>
           <EqualButton operation={operation} value1={value1} value2={value2} equalOnClick={equalOnClick}/>
         </div>
-        
-        <h1>Output: {output}</h1>
-        <h2>Value1: {value1}</h2>
-        <h2>Operation: {operation}</h2>
-        <h2>Value2: {value2}</h2>
     </div>
   )
 }
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,8 +1,17 @@
 import React from 'react'
 
-function Display() {
+function Display({value1, value2, operation, output}) {
+  if (value1 && !value2) {
+    return <div className='display'>{value1}</div>
+  } else if (value2) {
+    return <div className='display'>{value2}</div>
+  } else if (output) {
+    return <div className='display'>{output}</div>
+  }
+
+
   return (
-    <div>Display</div>
+    <div className='display'></div>
   )
 }
 
